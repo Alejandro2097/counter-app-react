fee71018cc8bb399c3b9e0d5b1928cf0a78f8556
Alejandro2097 <alejo12381@hotmail.es>
2023-04-07 23:10:17 -0500

    rafc

--- src/HelloWorldApp.jsx
+++ src/HelloWorldApp.jsx
@@ -1,4 +1,14 @@
 
-export function App() {
-    return (<h1>Hola mundo!!!</h1>);
+// function App() {
+//     return (<h1>Hola mundo</h1>);
+// }
+
+// export default App;
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <div>HelloWorldApp</div>
+  )
 }
