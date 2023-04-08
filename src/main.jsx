d0c546aded81dfb5081c4ad9d1ae541528e199b5
Alejandro2097 <alejo12381@hotmail.es>
2023-04-07 22:59:45 -0500

    render

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+function App() {
+    return (<h1>Hola mundo</h1>);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render( 
+    
+)
