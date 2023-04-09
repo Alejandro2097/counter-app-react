44d72d41d356786ae1c02bd49b8aab2caab55daa
Alejandro2097 <alejo12381@hotmail.es>
2023-04-08 23:53:57 -0500

    hangle add

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({value}) => {
+
+    function handleAdd(event, newValue) {
+        // console.log(event)
+        console.log(newValue);
+    }
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
-            <button>
+            <button onClick={(event) => handleAdd(event, 'hola')}>
                 + 1
             </button>
         </>
