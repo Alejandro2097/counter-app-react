faa43baeae0c901b870295ca95d4ad45822061e8
Alejandro2097 <alejo12381@hotmail.es>
2023-04-08 23:43:15 -0500

    counter

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({ title, subTitle, name}) => {
    
     // console.log(title);
 
@@ -9,6 +9,7 @@ export const FirstApp = ({title, subTitle}) => {
             <h1>{title}</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
             <p>{subTitle}</p>
+            <p>{name}</p>
         </>
         
     )
@@ -17,4 +18,10 @@ export const FirstApp = ({title, subTitle}) => {
 FirstApp.propTypes = {
     title: PropTypes.string.isRequired,
     subTitle: PropTypes.number.isRequired,
+}
+
+FirstApp.defaultProps = {
+    name: 'Alejadnro Huertas',
+    subTitle: 123,
+    title: 'No hay titulo',
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import { FirstApp } from './FirstApp'
 import { HelloWorldApp } from './HelloWorldApp'
+
 import './styles.css';
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
     <React.StrictMode>
-        <FirstApp title="Hola Soy goku" subTitle={123}/>
+        <CounterApp value={45}/>
     </React.StrictMode>
 )
