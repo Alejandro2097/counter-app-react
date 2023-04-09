e5059518f87c74e187a093726105afbf03f9b63c
Alejandro2097 <alejo12381@hotmail.es>
2023-04-09 16:18:27 -0500

    template string

--- src/base-pruebas/02-template-string.js
+++ src/base-pruebas/02-template-string.js
@@ -1,16 +1,3 @@
-
-
-
-const nombre   = 'Fernando';
-const apellido = 'Herrera';
-
-
-// const nombreCompleto = nombre + ' ' + apellido;
-const nombreCompleto = `${ nombre } ${ apellido }`;
-
-console.log( nombreCompleto );
-
-
 function getSaludo(nombre) {
     return 'Hola ' + nombre;
 }
--- test/demo.test.js
+++ test/demo.test.js
@@ -1,8 +1,10 @@
-test('Esta prueba no debe de fallar', () => {
-    // 1. inicializacion 
-    const message1 = 'Hola Mundo';
-    // 2. Estimulo
-    const message2 = message1.trim()
-    // 3. Observar el comportamiento
-    expect(message1).toBe(message2);
+describe('Pruebas en <DemoComponent2/>' ,  () => {
+    test('Esta prueba no debe de fallar', () => {
+        // 1. inicializacion 
+        const message1 = 'Hola Mundo';
+        // 2. Estimulo
+        const message2 = message1.trim()
+        // 3. Observar el comportamiento
+        expect(message1).toBe(message2);
+    });
 });
