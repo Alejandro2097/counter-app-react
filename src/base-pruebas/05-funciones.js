643e622f54dd2d2a8fe7aff88adeb0cfad465be0
Alejandro2097 <alejo12381@hotmail.es>
2023-04-09 17:00:53 -0500

    funciones tested

--- src/base-pruebas/05-funciones.js
+++ src/base-pruebas/05-funciones.js
@@ -1,10 +1,10 @@
 
-const getUser = () => ({
+export const getUser = () => ({
         uid: 'ABC123',
         username: 'El_Papi1502'
 });
 // Tarea
-const getUsuarioActivo = ( nombre ) =>({
+export const getUsuarioActivo = ( nombre ) =>({
     uid: 'ABC567',
     username: nombre
 })
