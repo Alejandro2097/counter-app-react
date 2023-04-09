354aef3443890616620890a46fb65a8cda6d4f19
Alejandro2097 <alejo12381@hotmail.es>
2023-04-09 15:53:14 -0500

    types

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,3 +1,8 @@
 test('Esta prueba no debe de fallar', () => {
-    
+    // 1. inicializacion 
+    const message1 = 'Hola Mundo';
+    // 2. Estimulo
+    const message2 = message1.trim()
+    // 3. Observar el comportamiento
+    expect(message1).toBe(message2);
 });
