d2c836a9cfad3ffa4bd125d04816cf37452d757b
Alejandro2097 <alejo12381@hotmail.es>
2023-04-09 17:04:56 -0500

    deses arr test

---
+++ test/07-deses-arr.test.js
@@ -0,0 +1,8 @@
+import { retornaArreglo } from "../src/base-pruebas/07-deses-arr";
+
+
+describe('Pruebas en 07-deses-arr', () => {
+    test('debe de retornar un String y un numero ', () => {
+        const retorno = retornaArreglo();
+    });
+});
