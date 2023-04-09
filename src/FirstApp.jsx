08a69d14f621fe379f3dfd7fb533acd9f470fa9a
Alejandro2097 <alejo12381@hotmail.es>
2023-04-08 21:43:32 -0500

    row

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Fragment } from 'react'
 
 export const FirstApp = () => {
     return  (
-        <div>
+        <>
             <h1>Alejandro Krauser First React app</h1>
             <p>Some things</p>
-        </div>
+        </>
         
     )
 }
