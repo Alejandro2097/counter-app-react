bb2f2211d9eb4b30bb1b87496237bb3a17ba6ca1
Alejandro2097 <alejo12381@hotmail.es>
2023-04-10 21:32:00 -0500

    seriusly pls

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { CounterApp } from './CounterApp';
 import { FirstApp } from './FirstApp'
 import { HelloWorldApp } from './HelloWorldApp'
 
 import './styles.css';
-import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
     <React.StrictMode>
-        <FirstApp />
+        <FirstApp  title="Hola soy Vegueta"/>
     </React.StrictMode>
 )
