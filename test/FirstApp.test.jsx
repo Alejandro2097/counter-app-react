ea7020c49f7b4d58e97a864231d37310a80f5bad
Alejandro2097 <alejo12381@hotmail.es>
2023-04-12 22:30:59 -0500

    getting started

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -1,8 +1,11 @@
 import { render } from "@testing-library/react";
+
 import { FirstApp } from "../src/FirstApp";
 
 describe('Pruebas en <FirstApp/>', () => {
     test('debe hacer match con el snapshot', () => {
-        render(<FirstApp/>)
+        const title = 'Hola soy goku'
+        const { container } =  render(<FirstApp title={title}/>)
+        expect(container).toMatchSnapshot()
     });
 });
