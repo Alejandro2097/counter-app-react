de9217f2b07ab0cd42114df0d8176bc4cc7c851c
Alejandro2097 <alejo12381@hotmail.es>
2023-04-12 22:39:32 -0500

    h1

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -10,6 +10,8 @@ describe('Pruebas en <FirstApp/>', () => {
     });
     test('debe hacer match con el snapshot', () => {
         const title = 'Hola soy goku'
-        const { container } =  render(<FirstApp title={title}/>)
+        const { container, getByText } =  render(<FirstApp title={title}/>)
+        expect(getByText(title)).toBeTruthy();
+        const h1  = container.querySelector('h1');
     });
 });
