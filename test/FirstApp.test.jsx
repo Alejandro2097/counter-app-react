ca8fbf87bd1ae00a5e9d1b30623cf8609b0fb907
Alejandro2097 <alejo12381@hotmail.es>
2023-04-13 23:27:45 -0500

    uh

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -13,5 +13,6 @@ describe('Pruebas en <FirstApp/>', () => {
         const { container, getByText } =  render(<FirstApp title={title}/>)
         expect(getByText(title)).toBeTruthy();
         const h1  = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
     });
 });
